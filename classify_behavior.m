ca678378d3be2cba3fd9a29d1f8e7ec28791c018
robert-giaquinto <giaquinto.ra@gmail.com>
2015-03-29 23:22:30 -0500

    Began the next big phase of the project: object behavior classification.
    Including two new functions:
    	1. read_csvs.m - reads in all the csv files in a directory
    		and appends them in a matlab table
    
    	2. classify_behavior.m - began using the data in the csv files
    		to classify behavior. For starters I created some new
    		features based on distance between agents. I also began testing
    		out a boosting approach to classification which can be pretty fast
    		and fairly accurate.

---
+++ classify_behavior.m
@@ -0,0 +1,97 @@
+clear;
+
+% Implement a classification of object behavior
+% In final system, will rely on Tom's work for locations
+% in the meantime use the data that creates the GIFs
+
+% 1. Import all of the csv files
+user_name = strtrim(char(java.lang.System.getProperty('user.name')));
+if strcmp(user_name, 'robert')
+    cd('/Users/robert/documents/UMN/5561_CV/project/code');
+    data_dir = '/Users/robert/documents/UMN/5561_CV/project/data/';
+else
+    cd('/Users/robert/documents/MATLAB/...');
+    data_dir = '/Users/Tom/documents/.../data/';
+end
+file_names = dir(strcat(data_dir, 'follow_*.csv'));
+all_data = read_csvs(data_dir, file_names);
+
+
+% 2. put positions of each agent as separate variables
+% identify other agents
+agent_names = unique(all_data.agent);
+others = agent_names(strcmp(agent_names, 'one') == 0);
+num_other_agents = size(others, 1);
+% initialize new wider table
+data = all_data(strcmp(all_data.agent, 'one'),:);
+var_names = all_data.Properties.VariableNames;
+num_vars = size(var_names, 2);
+% loop through other agents table and add it to the data table
+for i=1:num_other_agents
+    other_name = others(i);
+    data_other = all_data(strcmp(all_data.agent, other_name),:);
+    % drop action since we cannot know this, and agent name
+    data_other.action = [];
+    data_other.agent = [];
+    % modify variable names to distinguish them from agent one
+    for v=1:num_vars-2
+        data_other.Properties.VariableNames(v) = strcat(...
+            data_other.Properties.VariableNames(v), ...
+            '_', other_name);
+    end
+    data = innerjoin(data, data_other, 'LeftKeys', 1, 'RightKeys', 1); 
+end
+data.agent = [];
+% change action variable to binary
+data.target = zeros(size(data, 1), 1);
+data.target(strcmp(data.action, 'cruising') == 0) = 1;
+data.action = [];
+
+% 3. transform other agents positions into features
+var_names = data.Properties.VariableNames;
+num_vars = size(var_names, 2);
+for i=1:num_other_agents
+    % find difference in x distance
+    newx_var_name = strcat('one_', others(i), '_x_dist');
+    other_x_var_name = strcat('x_pos_', others(i));
+    data{:, newx_var_name} = abs(data.x_pos - data{:, other_x_var_name});
+    % distance in y
+    newy_var_name = strcat('one_', others(i), '_y_dist');
+    other_y_var_name = strcat('y_pos_', others(i));
+    data{:, newy_var_name} = abs(data.y_pos - data{:, other_y_var_name});
+    
+end
+
+% split into test and training data
+% TBD
+pred_vars = {'x_pos', 'y_pos', 'x_pos_two', 'y_pos_two', 'one_two_x_dist', 'one_two_y_dist'};
+x = data{:, pred_vars};
+y = data.target;
+cvpart = cvpartition(data.target, 'holdout', 0.3);
+x_train = x(training(cvpart), :);
+y_train = y(training(cvpart), :);
+x_test = x(test(cvpart), :);
+y_test = y(test(cvpart), :);
+
+% fit model on training data
+boost = fitensemble(x_train, y_train, 'AdaBoostM1', 250, 'Tree');
+pred = predict(boost, x_test);
+
+figure;
+plot(loss(boost, x_test, y_test, 'mode', 'cumulative'));
+xlabel('Number of trees');
+ylabel('Test classification error');
+
+cv = fitensemble(x_train, y_train, 'AdaBoostM1', 200, 'Tree', ...
+    'type', 'classification', 'kfold', 5);
+figure;
+plot(loss(boost, x_test, y_test, 'mode', 'cumulative'));
+hold on;
+plot(kfoldLoss(cv, 'mode', 'cumulative'), 'r.');
+hold off;
+xlabel('Number of trees');
+ylabel('Classification error');
+legend('Test','Cross-validation','Location','NE');
+
+
+
---
+++ read_csvs.m
@@ -0,0 +1,28 @@
+function data = read_csvs(data_dir, file_names)
+% data_dir: char, path to csv files
+% file_names an object created by dir function
+num_files = size(file_names, 1);
+if num_files == 1
+    file_name = strcat(data_dir, file_names(1).name);
+    data = readtable(file_name);
+elseif num_files > 1
+    for f = 1:num_files
+        file_name = strcat(data_dir, file_names(f).name);
+        % read in data as a table
+        temp = readtable(file_name);
+        if f == 1
+            % initialize data table as cell array
+            data = cell(size(temp,1) * num_files, size(temp,2));
+        end
+        % save the data in preallocated cell array
+        row_start = ((f-1) * size(temp,1)) + 1;
+        row_end = f * size(temp,1);
+        data(row_start:row_end, :) = table2cell(temp);
+    end
+    data = cell2table(data);
+    data.Properties.VariableNames = temp.Properties.VariableNames;
+else
+    disp('zero files found');
+    data = table();
+end
+end
