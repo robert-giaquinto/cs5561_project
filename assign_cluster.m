04dd4c4c0acc34595b1043b003ca519dc673423d
robert-giaquinto <giaquinto.ra@gmail.com>
2015-05-06 20:48:16 -0500

    Added a utility function to assign new data points to a known cluster (assign_cluster.m)

---
+++ assign_cluster.m
@@ -0,0 +1,20 @@
+function labels = assign_cluster(cluster_centers, x)
+
+num_points = size(x, 1);
+num_clusters = size(cluster_centers, 1);
+labels = zeros([num_points, 1]);
+
+% loop through each cluster, keep note of closest cluster
+best_ssd = ones(num_points, 1) * inf;
+for k = 1:num_clusters
+    ssd = sum_squared_distance(x, cluster_centers(k, :));
+    for n = 1:num_points
+        if ssd(n) < best_ssd(n)
+            labels(n) = k;
+            best_ssd(n) = ssd(n);
+        end
+    end
+end
+
+
+end
